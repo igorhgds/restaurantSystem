d34b839f6b86c318967f12e6647a9c2c2f155f0f
igorhgds <igorhenrique.gomes@hotmail.com>
2025-05-15 13:04:38 -0300

    feat: created entity Dish and migration

--- src/main/java/igor/henrique/entities/Dish.java
+++ src/main/java/igor/henrique/entities/Dish.java
@@ -1,46 +1,42 @@
 package igor.henrique.entities;
 
-import igor.henrique.enums.TableStatus;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.PrePersist;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-@Entity(name = "tables")
+@Entity(name = "dishes")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class Table {
+public class Dish {
+
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "table_number", nullable = false)
-    private int tableNumber;
+    @Column(nullable = false, unique = true)
+    private String name;
 
-    @Column(name = "table_status", nullable = false)
-    @Enumerated(EnumType.STRING)
-    private TableStatus tableStatus;
+    @Column(length = 500)
+    private String description;
 
-    @Column(name = "created_at", nullable = false)
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    @Column(nullable = false)
     private LocalDateTime createdAt;
-
-    @ManyToOne
-    @JoinColumn(name = "waiter_id", nullable = false)
-    private User waiter;
 
     @PrePersist
     public void prePersist() {
---
+++ src/main/resources/db.migration/V3__create_dish_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE dishes (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255) NOT NULL UNIQUE,
+        description VARCHAR(500),
+        price NUMERIC(10, 2) NOT NULL,
+        created_at TIMESTAMP NOT NULL
+);
