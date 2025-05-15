1e3b062838d3851ef337d6271ce0e5b3777dc27e
igorhgds <igorhenrique.gomes@hotmail.com>
2025-05-15 09:37:44 -0300

    feat: update base project

---
+++ src/test/java/igor/henrique/RestaurantSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package igor.henrique;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
