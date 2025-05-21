48f1c26cf72f4505d2d7d833464c74812ecb9f1a
igorhgds <igorhenrique.gomes@hotmail.com>
2025-05-21 09:44:38 -0300

    feat: created TableJpaRepository

---
+++ src/main/java/igor/henrique/repositories/TableJpaRepository.java
@@ -0,0 +1,12 @@
+package igor.henrique.repositories;
+
+import igor.henrique.entities.Table;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface TableJpaRepository extends JpaRepository<Table, Long> {
+
+    Optional<Table> fingByTableNumber(int tableNumber);
+
+}
