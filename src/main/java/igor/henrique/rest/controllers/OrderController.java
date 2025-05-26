6404dd1bcd60f75ccefed465ec0ef58218371979
igorhgds <igorhenrique.gomes@hotmail.com>
2025-05-26 12:18:54 -0300

    feat: implemented CloseOrderUseCase in OrderController

--- src/main/java/igor/henrique/rest/controllers/OrderController.java
+++ src/main/java/igor/henrique/rest/controllers/OrderController.java
@@ -2,11 +2,14 @@ package igor.henrique.rest.controllers;
 
 import igor.henrique.dtos.order.input.CreateOrderInputDTO;
 import igor.henrique.dtos.order.output.OrderOutputDTO;
+import igor.henrique.usecases.order.CloseOrderUseCase;
 import igor.henrique.usecases.order.CreateOrderUseCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,10 +20,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrderController {
 
     private final CreateOrderUseCase createOrderUseCase;
+    private final CloseOrderUseCase closeOrderUseCase;
 
     @PostMapping
     public ResponseEntity<OrderOutputDTO> createOrder(@RequestBody CreateOrderInputDTO dto) {
         OrderOutputDTO response = createOrderUseCase.createOrder(dto);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
+
+    @PutMapping("/{id}/close")
+    public ResponseEntity<Void> closeOrder(@PathVariable Long id) {
+        closeOrderUseCase.closeOrder(id);
+        return ResponseEntity.noContent().build();
+    }
+
+
 }
