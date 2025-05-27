849f900697dca4a86c59de4f2fe721b8401f1111
igorhgds <igorhenrique.gomes@hotmail.com>
2025-05-27 10:46:48 -0300

    feat: created CreateDishInputDTO

--- src/main/java/igor/henrique/dtos/dish/input/CreateDishInputDTO.java
+++ src/main/java/igor/henrique/dtos/dish/input/CreateDishInputDTO.java
@@ -1,4 +1,18 @@
 package igor.henrique.dtos.dish.input;
 
-public record CreateDishInputDTO() {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.math.BigDecimal;
+
+public record CreateDishInputDTO(
+        @NotBlank
+        String name,
+
+        @NotNull
+        String description,
+
+        @NotNull
+        BigDecimal price
+) {
 }
