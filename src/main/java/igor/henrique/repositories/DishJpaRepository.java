87884d000de604a134143e5d9b5ff5d4049bbd10
igorhgds <igorhenrique.gomes@hotmail.com>
2025-05-27 11:50:50 -0300

    feat: created CreateDishUseCase

--- src/main/java/igor/henrique/repositories/DishJpaRepository.java
+++ src/main/java/igor/henrique/repositories/DishJpaRepository.java
@@ -1,6 +1,7 @@
 package igor.henrique.repositories;
 
 import igor.henrique.entities.Dish;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -8,4 +9,6 @@ import java.util.Optional;
 public interface DishJpaRepository extends JpaRepository<Dish, Long> {
 
     Optional<Dish> findByDishId(long dishId);
+
+    Optional<Object> findByName(@NotBlank String name);
 }
---
+++ src/main/java/igor/henrique/usecases/dish/CreateDishUseCase.java
@@ -0,0 +1,34 @@
+package igor.henrique.usecases.dish;
+
+import igor.henrique.dtos.dish.input.CreateDishInputDTO;
+import igor.henrique.dtos.dish.output.DishOutputDTO;
+import igor.henrique.entities.Dish;
+import igor.henrique.mappers.dish.DishStructMapper;
+import igor.henrique.repositories.DishJpaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springdoc.api.AbstractOpenApiResource;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CreateDishUseCase {
+
+    private final DishJpaRepository dishJpaRepository;
+    private final DishStructMapper dishStructMapper;
+    private final AbstractOpenApiResource abstractOpenApiResource;
+
+    public DishOutputDTO createDish(CreateDishInputDTO dto){
+
+        dishJpaRepository.findByName(dto.name()).ifPresent(existing -> {
+            throw new IllegalArgumentException("Já existe um prato com esse nome.");
+        });
+
+        Dish dish = dishStructMapper.toEntity(dto);
+        dish.setName(dto.name());
+        dish.setDescription(dto.description());
+        dish.setPrice(dto.price());
+
+        return dishStructMapper.toDishOutputDTO(dishJpaRepository.save(dish));
+
+    }
+}
